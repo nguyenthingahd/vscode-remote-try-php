7f9f6e7eb07b437829e8877a5f8736a4260d0ad2
Nguyen Thi Nga <100331906+nguyenthingahd@users.noreply.github.com>
2024-05-12 21:29:15 +0700

    Update LoginController.php

---
+++ QLSACH/controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+class LoginController {
+    public function index() {
+        // Hiển thị trang đăng nhập
+        include '../views/login/index.php';
+    }
+
+    public function login($db) {
+        require_once '../models/User.php';
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $userModel = new User($db);
+            $result = $userModel->login($username, $password);
+
+            if ($result) {
+                session_start();
+                $_SESSION['username'] = $username;
+                header("location: ../controllers/SachController.php");
+            } else {
+                header("location: ../controllers/LoginController.php?error=1");
+            }
+        }
+    }
+}
+?>
